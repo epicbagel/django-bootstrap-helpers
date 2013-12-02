a9a45f8e1af0439a50bcec3bcbd3caea1b4d370a
Jon Bolt <epic.bagel@gmail.com>
2013-12-02 10:23:44 +0000

    Init

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 import bootstrap_helpers
-setup(name='django-test-views',
+setup(name='django-bootstrap-helpers',
 	version=bootstrap_helpers.__version__,
 	packages = find_packages(),
 	license='The MIT License',
 	platforms=['OS Independent'],
-	keywords='django, testing',
+	keywords='django, bootstrap',
 	author='Jon Bolt',
 	author_email='jon@epicbagel.com',
 	url="https://github.com/epicbagel/django-bootstrap-helpers",
